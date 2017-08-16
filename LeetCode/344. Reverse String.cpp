37c6f6111c7ed355c724880e628ffd530aacf4db
Tuzki <fangxinzhe@hotmail.com>
2017-08-16 20:25:00 +0800

    2017/08/16

---
+++ LeetCode/344. Reverse String.cpp
@@ -0,0 +1,15 @@
+/*344. Reverse String
+Write a function that takes a string as input and returns the string reversed.
+
+Example:
+Given s = "hello", return "olleh".*/
+class Solution {
+public:
+    string reverseString(string s) {
+        int i = 0, j = s.length() - 1;
+        while(i < j){
+            swap(s[i++], s[j--]); 
+        }  
+        return s;
+    }
+};
